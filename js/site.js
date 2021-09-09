91010086faaa04b9e9a11165d6e1721121f437f8
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-09-08 21:25:56 -0400

    app layout and started js code
    most of app page layout completed. started js code for app

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-// call helloworld
-function HelloWorld() {
-    alert("hello world");
+// get user values
+// controller
+function getValues() {
+    let loanamount = document.getElementById("inputLoanAmount").value;
+    let payments = document.getElementById("inputPayments").value;
+    let rate = document.getElementById("inputRate").value;
+
+    // convert to int
+    loanamount = parseInt(loanamount);
+    payments = parseInt(payments);
+    rate = parseInt(rate);
+
+    // validate is integer
+    if (Number.isInteger(loanamount) && Number.isInteger(payments) && Number.isInteger(rate)) {
+        // call calculate payment
+        let calc = calculatePayments(loanamount, payments, rate);
+    }
 }
+
+
+// calculate payments
+// logic
+function calculatePayments(la, p, r) {
+    let calc = {};
+
+    calc.totalMonthlyPayment = la * (r/1200) / (1 - (1 + r/1200)**p);
+    //calc.totalInterest = 
+}
+
+
+// display payments and amortization table
+// view
+
